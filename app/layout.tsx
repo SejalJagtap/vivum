a88655e32553ec013fd65aed845f3a90b4f7a70b
Sejal Jagtap <sejaljagtap1309@gmail.com>
2024-05-08 04:50:38 +0530

    Vivum ai pnpm remove

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ export const metadata = {
     ? new URL(`https://${process.env.VERCEL_URL}`)
     : undefined,
   title: {
-    default: 'Next.js AI Chatbot',
-    template: `%s - Next.js AI Chatbot`
+    default: 'Vivum AI Chatbot',
+    template: `%s - Vivum AI Chatbot`
   },
   description: 'An AI-powered chatbot template built with Next.js and Vercel.',
   icons: {
